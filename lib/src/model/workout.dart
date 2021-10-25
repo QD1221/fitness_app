3645d47f45e2ba7dd6ff0832f9bf1a004d02ecee
QD1221 <zxcvbnm12122000@gmail.com>
2021-10-25 08:54:33 +0700

    2nd commit

---
+++ lib/src/model/workout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Workout {
+  String? thumbImg;
+  String? title;
+  String? subtitle;
+  String? time;
+
+  Workout({
+    this.thumbImg,
+    this.title,
+    this.subtitle,
+    this.time,
+  });
+}
--- lib/src/ui/main_page.dart
+++ lib/src/ui/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:coolicons/coolicons.dart';
+import 'package:fitness_app/src/data/feature_workouts.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -30,7 +32,13 @@ class _MainPageState extends State<MainPage> {
                         ),
                       ),
                       SizedBox(height: 4),
-                      Text('You\'ve 2 free trial class'),
+                      Text(
+                        'You\'ve 2 free trial class',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
                   const Spacer(),
@@ -49,18 +57,161 @@ class _MainPageState extends State<MainPage> {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Container(
+              child: SizedBox(
                 height: 280,
-                child: Placeholder(),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Featured Workouts',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Expanded(
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: featureWorkoutItems.length,
+                          itemBuilder: (context, index) {
+                            var item = featureWorkoutItems[index];
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 16),
+                              child: SizedBox(
+                                width: 150,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      height: 180,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image:
+                                              NetworkImage(item.thumbImg ?? ''),
+                                          fit: BoxFit.cover,
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(
+                                              item.title ?? '-',
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            const Spacer(),
+                                            Text(
+                                              item.time ?? '-',
+                                              style: const TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        const SizedBox(height: 8),
+                                        Text(
+                                          item.subtitle ?? '-',
+                                          style: const TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 13,
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
-            Container(
-              height: 84,
-              child: Placeholder(),
+            SizedBox(
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Workout Progress',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          '12 class ongoing',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: CircularPercentIndicator(
+                        radius: 44,
+                        progressColor: Colors.black,
+                        percent: 0.75,
+                        center: const Text(
+                          '75%',
+                          style: TextStyle(fontSize: 11),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Expanded(
-              child: Placeholder(),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                    right: 120,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.lightGreenAccent,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(32),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 64,
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 120,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
